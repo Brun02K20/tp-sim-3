f63b12342d78581460c9897d30376d543637b921
Brun02K23 <bvirinni@gmail.com>
2024-04-25 16:38:58 -0300

    politica de CORS

--- api/src/main/java/sim/tps/api/controllers/MontecarloController.java
+++ api/src/main/java/sim/tps/api/controllers/MontecarloController.java
@@ -3,10 +3,7 @@ package sim.tps.api.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sim.tps.api.dto.RequestDto;
 import sim.tps.api.dto.VectorEstado;
 import sim.tps.api.services.MontecarloService;
@@ -16,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/montecarlo")
 @RequiredArgsConstructor
+@CrossOrigin(origins= "*")
 public class MontecarloController {
 
     private final MontecarloService montecarloService;
